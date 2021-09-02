590c3f92e51eb1746f069e611a61b88730d69e4d
Tariq Ayman <tariq.ayman94@gmail.com>
2021-09-02 14:09:15 +0200

    create routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,19 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->group(['prefix' => 'api/'], function ($app) {
+
+    $app->post('register', 'AuthController@register');
+    $app->post('login', 'AuthController@login');
+    $app->get('verify/{token}', 'AuthController@verifyAccount');
+
+    $app->group(['middleware' => ['auth','IsVerifyEmail']], function ($app) {
+        $app->post('article', 'ArticlesController@store');
+        $app->get('article', 'ArticlesController@index');
+        $app->get('article/{id}', 'ArticlesController@show');
+        $app->put('article/{id}', 'ArticlesController@update');
+        $app->delete('article/{id}', 'ArticlesController@destroy');
+    });
+
+});
